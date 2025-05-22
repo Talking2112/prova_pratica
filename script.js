844263cdfe0753306ebaf0c4daa76752346b2100
GABRIEL MARTIN PADILHA CARADA <gabriel_carada@estudante.sesisenai.org.br>
2025-05-22 09:25:42 -0300

    Adicionar log

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const PORTA = 8000
 const fs = require('fs')
 
+const PORTA = 8000
+const server = express()
+server.use(express.json())
+
 function adicionarMensagem (nome_aluno) {
-    const mensagem = `${id_unico} - ${data_hora} - ${nome_aluno}\n`
     const data_hora = new Date().toISOString()
     const id_unico = Math.random().toString(36).substring(2, 10)
+    const mensagem = `${id_unico} - ${data_hora} - ${nome_aluno}\n`
 
-    fs.appendFile('logs.text', mensagem, (err) => {
+    fs.appendFile('logs.txt', mensagem, (err) => {
         if (err) {
             console.error("Erro ao adcionar", err)
         }else{
@@ -16,8 +19,14 @@ function adicionarMensagem (nome_aluno) {
     })
 }
 
-const server = express()
-server.use(express.json())
-
+server.post('/logs', (req, res) => {
+    const {nome_aluno} = req.body
+        if(nome_aluno){
+            return res.status(400).json({erro: 'Você não colocou um nome!'})
+        }
+        adicionarMensagem(nome_aluno)
+            res.status(200).json({mensagem: 'Log salvo'})
+    
+})
 
 server.listen(PORTA, ()=> {console.log("Servidor rodando na porta 8000.")})
