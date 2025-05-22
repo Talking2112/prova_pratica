bc32234946db9fa4074c6e5ffb7564484fcf3f45
GABRIEL MARTIN PADILHA CARADA <gabriel_carada@estudante.sesisenai.org.br>
2025-05-22 08:42:40 -0300

    Função de registro

---
+++ script.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+function adicionarMensagem (nome_aluno) {
+    const mensagem = `${id_unico} - ${data_hora} - ${nome_aluno}\n`
+    const data_hora = new Date().toISOString()
+    const id_unico = Math.random().toString(36).substring(2, 10)
+
+    fs.appendFile('logs.text', mensagem, (err) => {
+        if (err) {
+            console.error("Erro ao adcionar", err)
+        }else{
+            console.log("Log adcionado")
+        }
+    })
+}
